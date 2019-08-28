7d22cf7ddcd965369c9298adf176ccbc82f9b662
victor-ferraz7 <victor.fzn7@gmail.com>
2019-08-28 19:43:27 -0300

    Primeiro componente

---
+++ src/Tabela.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+const TableHead = () => {
+    return (
+        <thead>
+            <tr>
+                <th>Autores</th>
+                <th>Livros</th>
+                <th>Preços</th>
+                <th>Remover</th>
+            </tr>
+        </thead>
+
+    )
+}
+
+const TableBody = () => {
+    return (
+        <tbody>
+            <tr>
+                <td>Paulo</td>
+                <td>React</td>
+                <td>1000</td>
+                <td><button>Remover</button></td>
+            </tr>
+            <tr>
+                <td>Nico</td>
+                <td>React-Native</td>
+                <td>1000</td>
+                <td><button>Remover</button></td>
+            </tr>
+            <tr>
+                <td>Daniel</td>
+                <td>React</td>
+                <td>500</td>
+                <td><button>Remover</button></td>
+            </tr>
+        </tbody>
+    )
+}
+
+
+class Tabela extends Component {
+    render() {
+        return (
+            <table>
+                <TableHead />
+                <TableBody/>
+            </table>
+        )
+    }
+}
+
+export default Tabela;
